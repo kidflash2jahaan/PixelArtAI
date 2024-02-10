9f8c1bfcbd7dd12634c267fffb486ccb75b3fe21
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2024-02-10 11:42:20 -0800

    if not prompt or not size

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ def index():
     prompt = request.args.get("prompt")
     size = request.args.get("size")
 
+    if not prompt or not size:
+        return "Please provide a prompt and size."
+
     return img_creator(prompt, size)
 
 
